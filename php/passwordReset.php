e8fb0d00a889f1ab9ce7b003bdd7389a93daf44e
Jchau18 <160356687+Jchau18@users.noreply.github.com>
2024-03-09 14:04:48 -0600

    Update passwordReset.php

--- php/passwordReset.php
+++ php/passwordReset.php
@@ -18,19 +18,19 @@ session_start();
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get username and password from the form
+    // Get username and passwords from the user
     $username = $_POST['username'];
     $oldPassword = $_POST['oldPassword'];
     $newPassword = $_POST['newPassword'];
     $hash = password_hash($oldPassword, PASSWORD_BCRYPT);
 
-    // Prepare a SQL query to check if the username and password match
+    // Prepare a SQL query to check if the username and passwordHash match
     $sql = "SELECT * FROM Users WHERE Username = '$username' AND Hashpassword = '$hash'";
     $result = $conn->query($sql);
 
     // Check if any row is returned
     if ($result->num_rows > 0) {
-        // The username and password are correct, so set the session variable and redirect to the home page
+        // The username and password are correct, now we update the password has that matched with the username
         $result -> free_result();
         $hash = password_hash($newPassword, PASSWORD_BCRYPT);
         $sql = "UPDATE Users SET Hashpassword = '$hash' WHERE Username = $username;";

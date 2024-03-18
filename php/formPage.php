06e1fd8ae6792e53e3f6cdb95d349344bbc5df76
Jchau18 <chauj180@gmail.com>
2024-03-17 20:17:35 -0500

    Form

--- php/formPage.php
+++ php/formPage.php
@@ -27,7 +27,7 @@
          <?php
          //<a href="form.php?id=1">Open Form</a> Use this if you want a link to display the form 
          //The link will also give us a form id which we need to hard code I believe
-          $id = $_GET['id'];
+          //$id = $_GET['id'];
           $id = 1;
           // Database connection parameters
           $servername = "localhost"; // Change if your MySQL server is on a different host
@@ -44,7 +44,7 @@
           }
           
           // Prepare and execute SQL query to fetch data based on the ID
-          $sql = "SELECT QuestionID, QuestionText FROM questions WHERE id = ?";
+          $sql = "SELECT QuestionID, QuestionText FROM questions WHERE FormID = ?";
           $stmt = $conn->prepare($sql);
           $stmt->bind_param("i", $id);
           $stmt->execute();
@@ -54,14 +54,38 @@
         if ($result->num_rows > 0) {
           // Output data of each row
           while($row = $result->fetch_assoc()) {
-            echo "<p>$row["QuestionText"] . </p>";
+            echo "<p>" . $row["QuestionText"] . "</p>";
           }
         } else {
             echo "No item found with ID: " . $id;
           }
 
+        $stmt->close();
+        
+        //Displays answers
+        $sql2 = "SELECT  AnswerOption, AnswerID FROM answers WHERE FormID = ?";
+        $stmt2 = $conn->prepare($sql2);
+        $stmt2->bind_param("i", $id);
+        $stmt2->execute();
+        $result2 = $stmt2->get_result();
+
+          // Check if any row is returned
+          if ($result2->num_rows > 0) {
+            // Output radio buttons for each answer option
+            echo "<form>";
+            while ($row2 = $result2->fetch_assoc()) {
+                echo '<input type="radio" name="answer" value="' . $row2["AnswerID"] . '">' . $row2["AnswerOption"] . '<br>';
+            }
+            echo "</form>";
+        } else {
+            echo "No answers found for FormID: " . $id;
+        }
+          
+         
+
           // Close the database connection
-          conn->close();
+          $stmt2->close();
+          $conn->close();
           exit;
           ?>
 

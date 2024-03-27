d26ef1db1d2719964408e9df27a9f0f20c8e9a19
Jchau18 <chauj180@gmail.com>
2024-03-26 20:27:08 -0500

    asd

--- php/login.php
+++ php/login.php
@@ -21,35 +21,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get username and password from the form
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $hash = password_hash($password, PASSWORD_BCRYPT);
+    
 
     // Prepare a SQL query to check if the username and password match
     // WE NEED TO MAKE ALL PASSWORDS USE HASH SINCE WE DONT HAVE ANY SIGNUP PAGE. 
-    $sql = "SELECT * FROM users WHERE Username = '$username' AND Password = '$password'";
+    $sql = "SELECT * FROM users WHERE Username = '$username'";
     $result = $conn->query($sql);
 
-    // Check if any row is returned
-    if ($result->num_rows > 0) {
-        // Fetch the user ID
-        $row = $result->fetch_assoc();
-        $user_id = $row['UserID'];
+    // Runs through results
+    foreach($result as $r) {
+        // Using php function, we verify the hash that is on the DB
+        $pass_check = password_verify($password, $r['password']);
 
-        // Set the session variable
-        $_SESSION['UserID'] = $user_id;
-        
-        // Update UserID in the database
-        $update_sql = "UPDATE users SET UserID = ? WHERE Username = ?";
-        $update_stmt = $conn->prepare($update_sql);
-        $update_stmt->bind_param("is", $user_id, $username);
-        $update_stmt->execute();
-        $update_stmt->close();
-
-
-		header('Location: /499CAPSTONE/php/currentForms.php');
-		exit;
-    } else {
-        // Login failed
-        echo "Login failed. Please check your username and password.";
+        // If the password verify is true we login
+        if($pass_check) {
+		    $_SESSION['username'] = $username;
+		    header('Location: /499CAPSTONE/html/dashboard.html');
+		    exit;
+        } else {
+            // Login failed
+            echo "Login failed. Please check your username and password.";
+        }
     }
 }
 
